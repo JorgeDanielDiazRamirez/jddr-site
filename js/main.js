55316872e30fafd3fe727b0334c0c5c1d168f6d1
DESKTOP-P3OGUBB\Usuario <joddiazra@unal.edu.co>
2022-07-30 18:31:02 -0500

    Updates

--- js/main.js
+++ js/main.js
@@ -1,1 +1,37 @@
 console.log('working!!!');
+/*############# SHOW SIDEBAR ############*/
+
+/*######## Sidebar Show ########*/
+/* Validate if constant exists */
+
+/*######## Sidebar Hidden ########*/
+/* Validate if constant exists */
+
+/*############# SKILLS TABS ############*/
+const tabs = document.querySelectorAll('[data-target'),
+    tabContent = document.querySelectorAll('[data-content]')
+
+    tabs.forEach(tab => {
+        tab.addEventListener("click", () => {
+            const target = document.querySelector(tab.dataset.target)
+
+            tabContent.forEach(tabContents => {
+                tabContents.classList.remove("skills__active")
+            })
+
+            target.classList.add("skills__active")
+
+            tabs.forEach(tab => {
+                tab.classList.remove("skills__active")
+            })
+
+            tab.classList.add("skills__active")
+        })
+    })
+
+/*############# MIXITUP FILTER PORTFOLIO ############*/
+
+/*######## Link Acive Work ########*/
+/*######## Work popup ########*/
+
+/*#############  ############*/
